65c4016bab2eaa644bf422f23eac4efd4f747b88
Pascal Welke <pascal@xx>
2013-06-18 16:56:30 +0200

    output options, update of imports...

--- main.c
+++ main.c
@@ -4,12 +4,12 @@
 #include <string.h>
 #include <limits.h>
 
-#include "myconstants.h"
 #include "graph.h"
 #include "canonicalString.h"
 #include "loading.h"
 #include "listSpanningTrees.h"
 #include "main.h"
+#include "upperBoundsForSpanningTrees.h"
 /* #include "opk.h" */
 
 
@@ -17,15 +17,16 @@
  * Print --help message
  */
 void printHelp() {
-	printf("This is The Cyclic Pattern Kernel\n");
-	printf("implemented by Pascal Welke 2011/2012\n\n\n");
-	printf("usage: cpk F [parameterList]\n\n");
+	printf("This is The TreePatternKernel\n");
+	printf("implemented by Pascal Welke 2013\n\n\n");
+	printf("usage: tpk F [parameterList]\n\n");
 	printf("    without parameters: display this help screen\n\n");
 	printf("    F: (required) use F as graph database\n\n");
 	printf("    -input I: use input format I being\n\n");
 	printf("        no choice yet\n\n");
-	printf("    -depth d: choose size of subtrees that will be the features\n"
-		   "        (default 5)\n\n");
+	printf("    -filter d: choose maximum number of spanning trees\n"
+		   "        of a graph for which you are willing to compute\n"
+		   "        the kernel (default 1000)\n\n");
 	printf("    -label L: change labels\n\n");
 	printf("        n \"nothing\" (default) do not change anything\n"
 		   "        a \"active\" active - 1 | moderately active, inactive - -1\n"
@@ -36,6 +37,9 @@ void printHelp() {
 		   "            format specified by SVMlight the label of each graph has to be\n"
 		   "            either 0, 1 or -1 to be compliant with the specs of SVMlight.\n");
 	printf("        o returns if a graph is outerplanar\n"
+		   "        e returns the estimated number of spanning trees\n"
+		   "        s returns true number of spanning trees or -1 if\n"
+		   "            there are more than depth\n"
 		   "        t returns the time spent to compute the patterns\n"
 		   "        b returns the number of vertices in the BBTree\n"
 		   "        d returns the numbers of diagonals in each block\n\n");
@@ -112,7 +116,7 @@ int main(int argc, char** argv) {
 		char outputOption = 0;
 		char labelOption = 0;
 		int param;
-		int depth = 5;
+		int depth = 1000;
 
 		/* i counts the number of graphs read */
 		int i = 0;
@@ -158,7 +162,7 @@ int main(int argc, char** argv) {
 			/* if there was an error reading some graph the returned n will be -1 */
 			if (g->n > 0) {
 				
-				long int spanningTrees;
+				long int spanningTreeEstimate;
 
 				/* filter out moderately active molecules, if 'i' otherwise set labels */
 				if (labelOption == 'i') {
@@ -172,9 +176,19 @@ int main(int argc, char** argv) {
 					labelProcessing(g, labelOption);
 				}
 
-				spanningTrees = countSpanningTrees(g, 500, sgp, gp);
+				switch (outputOption) {
+					case 's':
+					spanningTreeEstimate = countSpanningTrees(g, depth, sgp, gp);
+					fprintf(stdout, "%i %li\n", g->number, spanningTreeEstimate);
+					break;
+					case 'e':
+					spanningTreeEstimate = getGoodEstimate(g, sgp, gp);
+					fprintf(stdout, "%i %li\n", g->number, spanningTreeEstimate);
+					break;
+				}
 				
-				printf("%i %li\n", g->number, spanningTrees);
+				
+				
 
 				if (i % 500 == 0) { fprintf(stderr, "."); }
 
