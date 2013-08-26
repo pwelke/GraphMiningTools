865b0c2924b0e0cf7b108be83d8b8e79431e1faf
Pascal Welke <pascal@xx>
2013-08-26 15:42:58 +0200

    added signatures to h file to speedup subtreeIsomorphismLabeled by merging matching into isomorphism check

--- bipartiteMatching.h
+++ bipartiteMatching.h
@@ -6,5 +6,8 @@ struct ShallowGraph* getMatching(struct Graph* g, struct ShallowGraphPool* sgp);
 struct ShallowGraph* bipartiteMatching(struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
 void addResidualEdges(struct Vertex* v, struct Vertex* w, struct ListPool* lp);
 void initBipartite(struct Graph* B);
+char augment(struct Vertex* s, struct Vertex* t);
+void removeSandT(struct Graph* B, struct Vertex* s, struct Vertex* t, struct GraphPool* gp);
+void setFlag(struct VertexList* e, int flag);
 
 #endif /* BIPARTITE_MATCHING_H_ */
