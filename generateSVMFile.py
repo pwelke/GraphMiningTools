88070d823f841f989b2411a52a7cd2402e660d57
Pascal Welke <pascal@xx>
2013-08-28 14:33:28 +0200

    updated documentation of the python code and a method creating the format needed by libsvm and co.

--- generateSVMFile.py
+++ generateSVMFile.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python
+
 import os
 
 def getMap(mapFile):
+	''' Given a filename of a file containing exactly one number per line, 
+	return a map that has these integers as keys and 1 as value for each.'''
 	map = dict()
 	f = open(mapFile, 'r')
 	for line in f:
@@ -9,6 +13,8 @@ def getMap(mapFile):
 	return map
 
 def getSet(mapFile):	
+	''' Given a filename of a file containing exactly one number per line, 
+	return a set that contains these integers.'''
 	map = set()
 	f = open(mapFile, 'r')
 	for line in f:
@@ -16,7 +22,10 @@ def getSet(mapFile):
 	f.close()
 	return map
 
-def filterBySet(file, set, outfile='results/counts.filtered'):
+def filterBySet(file, set, outfile):
+	''' Given a file that contains a pair of numbers per line and a set of integers,
+	write a file that contains only those lines, where the second number is contained
+	in the set.'''
 	f = open(file, 'r')
 	g = open(outfile, 'w')
 	for line in f:
@@ -27,6 +36,9 @@ def filterBySet(file, set, outfile='results/counts.filtered'):
 	g.close()
 
 def filterByMap(file, map, outfile='results/counts.filtered'):
+	''' Given a file that contains a pair of numbers per line and a map,
+	write a file that contains only those lines, where the second number is contained
+	in the key set of the map.'''
 	f = open(file, 'r')
 	g = open(outfile, 'w')
 	for line in f:
@@ -34,4 +46,25 @@ def filterByMap(file, map, outfile='results/counts.filtered'):
 		if pair[1] in map:
 			g.write(line)
 	f.close()
+	g.close()
+
+def putStuff(filteredfile, outfile):
+	map = dict()
+	
+	f = open(filteredfile, 'r')
+	for line in f:
+		pair = [int(x) for x in line.split()]
+		if pair[0] not in map:
+			map[pair[0]] = list()
+		map[pair[0]].append(pair[1])
+	f.close()
+
+	g = open(outfile, 'w')
+	keys = map.keys()
+	keys.sort()
+	for key in keys:
+		line = map[key]
+		line.sort()
+		g.write(' '.join([str(x) + ':1' for x in line]))
+		g.write('\n')
 	g.close()
