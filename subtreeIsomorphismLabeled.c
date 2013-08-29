69dbfb192e763dbe61faca76ac6b314f0d20bba3
Pascal Welke <pascal@xx>
2013-08-29 17:02:53 +0200

    added a new speedup level for subtreeCheck: LFF which does not remove edges from B but instead marks the vertex as "removed"

--- subtreeIsomorphismLabeled.c
+++ subtreeIsomorphismLabeled.c
@@ -499,3 +499,182 @@ char subtreeCheckLF(struct Graph* g, struct Graph* h, struct GraphPool* gp, stru
 
 	return 0;
 }
+
+
+/**
+dfs that searches for a path from s to t and augments it, 
+if found.
+returns 1 if there is a path or 0 otherwise.
+*/
+char augmentLFF(struct Vertex* s, struct Vertex* t) {
+	struct VertexList* e;
+
+	if (s == t) {
+		return 1;
+	}
+	s->visited = 1;
+	for (e=s->neighborhood; e!=NULL; e=e->next) {
+		if ((e->flag == 0) && (e->endPoint->visited == 0) && (e->endPoint->d == 0)) {
+			char found = augment(e->endPoint, t);
+			if (found) {
+				setFlag(e, 1);
+				s->visited = 0;
+				return 1;
+			}
+		}
+	}
+	s->visited = 0;
+	return 0;
+}
+
+
+/**
+Labeled Subtree isomorphism check. 
+Speedup to method above by reusing the whole bipartite matching utility graph B (including s and t) 
+
+Implements the version of subtree isomorphism algorithm described by
+
+Ron Shamir, Dekel Tsur [1999]: Faster Subtree Isomorphism. Section 2 
+
+in the labeled version
+*/
+char subtreeCheckLFF(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp) {
+	/* iterators */
+	int u, v;
+
+	struct Vertex* r = g->vertices[0];
+	int*** S = createCube(g->n, h->n);
+	int* postorder = getPostorderL(g, r->number);
+
+	struct CachedGraph* cacheB = initCachedGraph(gp, g->n);
+
+
+	/* init the S(v,u) for v and u leaves */
+	int* gLeaves = findLeaves(g, 0);
+	/* h is not rooted, thus every vertex with one neighbor is a leaf */
+	int* hLeaves = findLeaves(h, -1);
+	for (v=1; v<gLeaves[0]; ++v) {
+		for (u=1; u<hLeaves[0]; ++u) {
+			/* check compatibility of leaf labels */
+			if (strcmp(g->vertices[gLeaves[v]]->label, h->vertices[hLeaves[u]]->label) == 0) {
+				/* check for compatibility of edges */
+				if (strcmp(g->vertices[gLeaves[v]]->neighborhood->label, h->vertices[hLeaves[u]]->neighborhood->label) == 0) {
+					S[gLeaves[v]][hLeaves[u]] = malloc(2 * sizeof(int));
+					/* 'header' of array stores its length */
+					S[gLeaves[v]][hLeaves[u]][0] = 2;
+					/* the number of the unique neighbor of u in h*/
+					S[gLeaves[v]][hLeaves[u]][1] = h->vertices[hLeaves[u]]->neighborhood->endPoint->number;
+				}
+			}
+		}
+	}
+	/* garbage collection for init */
+	free(gLeaves);
+	free(hLeaves);
+	gLeaves = NULL;
+	hLeaves = NULL;
+
+	for (v=0; v<g->n; ++v) {
+		struct Vertex* current = g->vertices[postorder[v]];
+		int currentDegree = degree(current);
+		if ((currentDegree != 1) || (current->number == r->number)) {
+			for (u=0; u<h->n; ++u) {
+				int i;
+				int degU = degree(h->vertices[u]);
+				if (degU <= currentDegree + 1) {
+					/* if vertex labels match */
+					if (strcmp(h->vertices[u]->label, current->label) == 0) {
+						struct Graph* B = makeBipartiteInstanceLF(g, postorder[v], h, u, S, cacheB);
+						int* matchings = malloc((degU + 1) * sizeof(int));
+
+						int w;
+						struct Vertex* s = getVertex(gp->vertexPool);
+						struct Vertex* t = getVertex(gp->vertexPool);
+						s->number = -1;
+						t->number = -2;
+						
+						/* Add s, t and edges from s to A and from B to t.
+						Also, set residual capacities for these edges correctly */
+						for (w=0; w<B->number; ++w) {
+							addResidualEdges(s, B->vertices[w], gp->listPool);
+						}
+
+						for (w=B->number; w<B->n; ++w) {
+							addResidualEdges(B->vertices[w], t, gp->listPool);
+						}
+
+						matchings[0] = 0;
+						while (augmentLFF(s, t)) {
+							++matchings[0];
+						}
+
+						if (matchings[0] == degU) {
+							free(postorder);
+							free(matchings);
+							freeCube(S, g->n, h->n);
+							dumpGraph(gp, B);
+							return 1;
+						} else {
+							matchings[0] = degU + 1;
+						}
+
+						for (i=0; i<B->number; ++i) {
+							/* if the label of ith child of u is compatible to the label of the parent of v */
+							if ((current->lowPoint != -1) 
+								&& (strcmp(h->vertices[B->vertices[i]->lowPoint]->label, g->vertices[current->lowPoint]->label) == 0)) {
+								
+								struct VertexList* e;
+
+								/* remove vertex i from B and init B for matching algorithm */
+								// struct ShallowGraph* storage = removeVertexFromBipartiteInstanceLFF(B, i, s, sgp);
+								B->vertices[i]->d = 1;
+								initBipartite(B);
+								for (e=s->neighborhood; e!=NULL; e=e->next) {
+									setFlag(e, 0);
+								}
+								for (e=t->neighborhood; e!=NULL; e=e->next) {
+									setFlag(e, 1);
+								}
+								
+								/* run the matching algorithm */
+								matchings[i+1] = 0;	
+								while (augmentLFF(s, t)) {
+									++matchings[i+1];
+								}
+
+								/* convert output to S(u,v) format */
+								if (matchings[i+1] == degU - 1) {
+									matchings[i+1] = B->vertices[i]->lowPoint;
+								} else {
+									matchings[i+1] = -1;
+								}
+
+								// addVertexToBipartiteInstance(storage);
+								// dumpShallowGraph(sgp, storage);
+								B->vertices[i]->d = 0;
+							} else {
+								matchings[i+1] = -1;
+							}
+						}
+						S[current->number][u] = matchings;
+						
+						/* remove s and t, dump B */
+						dumpVertexListRecursively(gp->listPool, s->neighborhood);
+						dumpVertexListRecursively(gp->listPool, t->neighborhood);
+						dumpVertex(gp->vertexPool, s);
+						dumpVertex(gp->vertexPool, t);
+						//dumpGraph(gp, B);
+						returnCachedGraph(cacheB);
+					}
+				}
+			}		
+		}
+	}
+
+	/* garbage collection */
+	free(postorder);
+	freeCube(S, g->n, h->n);
+	dumpCachedGraph(cacheB);
+
+	return 0;
+}
--- subtreeIsomorphismLabeled.h
+++ subtreeIsomorphismLabeled.h
@@ -3,5 +3,6 @@
 
 char subtreeCheckL(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
 char subtreeCheckLF(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
+char subtreeCheckLFF(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
 
 #endif /* SUBTREE_ISOMORPHISM_H_ */
