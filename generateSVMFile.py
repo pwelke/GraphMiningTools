6efef00e7afac7dd927d72025f62ac6883a80510
Pascal Welke <pascal@xx>
2013-08-29 16:59:09 +0200

    working transformator utility that produces libsvm conforming data files

--- generateSVMFile.py
+++ generateSVMFile.py
@@ -48,7 +48,7 @@ def filterByMap(file, map, outfile='results/counts.filtered'):
 	f.close()
 	g.close()
 
-def putStuff(filteredfile, outfile):
+def createSVMFile(filteredfile, outfile):
 	''' Given filteredfile containing a pair of numbers x y per line, create a new file containing 
 	a sorted list of y's in each line given by x'''
 	map = dict()
@@ -67,6 +67,90 @@ def putStuff(filteredfile, outfile):
 	for key in keys:
 		line = map[key]
 		line.sort()
+		g.write(str(key) + ' ')
 		g.write(' '.join([str(x) + ':1' for x in line]))
 		g.write('\n')
 	g.close()
+
+def createLabeledSVMFile(filteredfile, outfile, labels):
+	''' Given filteredfile containing a pair of numbers x y per line, create a new file containing 
+	a sorted list of y's in each line given by x. First column contains a label given by label[x]'''
+	map = dict()
+	
+	f = open(filteredfile, 'r')
+	for line in f:
+		pair = [int(x) for x in line.split()]
+		if pair[0] not in map:
+			map[pair[0]] = list()
+		map[pair[0]].append(pair[1])
+	f.close()
+
+	g = open(outfile, 'w')
+	keys = map.keys()
+	keys.sort()
+	for key in keys:
+		line = map[key]
+		line.sort()
+		if labels[key] == 0:
+			continue
+		g.write(str(labels[key]) + ' ')
+		g.write(' '.join([str(x) + ':1' for x in line]))
+		g.write('\n')
+	g.close()
+
+def getLabels(infile):
+	''' Reads a file in the format of AIDS99.txt and returns a map
+	graph number -> activity'''
+	labels = dict()
+	f = open(infile, 'r')
+	for line in f:
+		if line.startswith('$'):
+			break
+		if line.startswith('#'):
+			data = line.split()
+			labels[int(data[1])] = int(data[2])
+	f.close()
+	return labels
+
+def changeLabels(labels, switch):
+	''' In labels, 
+
+	0 = inactive
+	1 = moderately active
+	2 = active
+
+	want to change that to 
+
+	1 = positive class
+	-1 = negative class
+	0 = filter out, when selecting.
+
+	thus, switch may be one of the following:
+
+	AvsI = 0->-1 2->1 1->0
+	AvsMI = 0->-1 1->-1 2->1
+	AMvsI = 0->-1 1->1 2->1'''
+	map = dict()
+	if switch == 'AvsI':
+		map[0] = -1
+		map[1] = 0
+		map[2] = 1
+
+	if switch == 'AvsMI':
+		map[0] = -1
+		map[1] = -1
+		map[2] = 1
+
+	if switch == 'AMvsI':
+		map[0] = -1
+		map[1] = 1
+		map[2] = 1
+
+	for key in labels.keys():
+		labels[key] = map[labels[key]]
+
+	return labels
+
+
+	
+
