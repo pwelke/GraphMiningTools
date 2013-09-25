8d7fe314adc767984edb495398a4c23c39b39420
Pascal Welke <pascal@xx>
2013-09-25 15:26:16 +0200

    added header info for new label array function

--- loading.h
+++ loading.h
@@ -4,6 +4,7 @@
 struct Graph* readSimpleFormat(char* filename, int undirected, struct GraphPool *p, int strspace);
 
 char* aids99VertexLabel(int label);
+char** aids99VertexLabelArray();
 char* aids99EdgeLabel(int label);
 struct Graph* iterateFile(char*(*getVertexLabel)(int), char*(*getEdgeLabel)(int));
 void createFileIterator(char* filename, struct GraphPool* p);
