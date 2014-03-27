413c708eeb07e25c42530f8784fb63cee2bf194b
Pascal Welke <pascal@xx>
2014-03-27 17:22:00 +0100

    ccd / countCycleDegree.c to count the cycle degree of vertices and graphs already working (and quite fast)

--- Makefile
+++ Makefile
@@ -3,13 +3,15 @@ LWMNAME = lwm
 MAPNAME = map
 CSCNAME = csc
 CPKNAME = cpk
+CCDNAME = ccd
 CPPFLAGS = -g -Wall -pedantic -W -ggdb -O3
 OBJECTS = $(patsubst %.c,%.o,$(wildcard *.c)) $(patsubst %.c,%.o,$(wildcard ./executables/*.c))
-TPKOBJECTS = $(filter-out ./executables/cpkMain.o ./executables/levelwiseMain.o ./executables/map2gaston.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
-LWMOBJECTS = $(filter-out ./executables/cpkMain.o ./executables/main.o ./executables/map2gaston.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
-MAPOBJECTS = $(filter-out ./executables/cpkMain.o ./executables/main.o ./executables/levelwiseMain.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
-CSCOBJECTS = $(filter-out ./executables/cpkMain.o ./executables/main.o ./executables/map2gaston.o ./executables/levelwiseMain.o, $(OBJECTS))
-CPKOBJECTS = $(filter-out ./executables/countSpanningTreeClasses.o ./executables/main.o ./executables/map2gaston.o ./executables/levelwiseMain.o, $(OBJECTS))
+TPKOBJECTS = $(filter-out ./executables/countCycleDegree.o ./executables/cpkMain.o ./executables/levelwiseMain.o ./executables/map2gaston.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
+LWMOBJECTS = $(filter-out ./executables/countCycleDegree.o ./executables/cpkMain.o ./executables/main.o ./executables/map2gaston.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
+MAPOBJECTS = $(filter-out ./executables/countCycleDegree.o ./executables/cpkMain.o ./executables/main.o ./executables/levelwiseMain.o ./executables/countSpanningTreeClasses.o, $(OBJECTS))
+CSCOBJECTS = $(filter-out ./executables/countCycleDegree.o ./executables/cpkMain.o ./executables/main.o ./executables/map2gaston.o ./executables/levelwiseMain.o, $(OBJECTS))
+CPKOBJECTS = $(filter-out ./executables/countCycleDegree.o ./executables/countSpanningTreeClasses.o ./executables/main.o ./executables/map2gaston.o ./executables/levelwiseMain.o, $(OBJECTS))
+CCDOBJECTS = $(filter-out ./executables/cpkMain.o  ./executables/countSpanningTreeClasses.o ./executables/main.o ./executables/map2gaston.o ./executables/levelwiseMain.o, $(OBJECTS))
 
 main: map
 	
@@ -35,6 +37,10 @@ map: $(MAPOBJECTS)
 csc: $(CSCOBJECTS)
 	echo "Link Count Graph Properties executable:"
 	gcc -o $(CSCNAME) $(CSCOBJECTS) $(CPPFLAGS)
+
+ccd: $(CCDOBJECTS)
+	echo "Link Count Graph Properties executable:"
+	gcc -o $(CCDNAME) $(CCDOBJECTS) $(CPPFLAGS)
 
 %.o : %.c %.h
 	gcc $(CPPFLAGS) -c $< -o $@
--- executables/countCycleDegree.c
+++ executables/countCycleDegree.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
+#include <malloc.h>
 
-#include "../graph.h"
-#include "../searchTree.h"
-#include "../listSpanningTrees.h"
-#include "../upperBoundsForSpanningTrees.h"
-#include "../subtreeIsomorphism.h"
-#include "../levelwiseMining.h"
-#include "levelwiseMain.h" 
-
-
+#include "../listComponents.h"
+#include "../loading.h"
+#include "../graphPrinting.h"
 
 /**
  * Print --help message
  */
 void printHelp() {
-	printf("This program counts the cycle degrees of vertices in a set of graphs\n");
+	printf("This program counts the cycle degrees of vertices in a set of graphs.\n");
+	printf("The cycle degree of a vertex is the number of biconnected components,\n");
+	printf("it is contained in. The cyc.deg of a graph G is the maximum over the\n");
+	printf("cycle degrees of all vertices in G. \n");
 	printf("implemented by Pascal Welke 2014\n\n\n");
 	printf("usage: [programName] F [parameterList]\n\n");
 	printf("    without parameters: display this help screen\n\n");
 	printf("    F: (required) use F as graph database\n\n");
 	printf("    -output O: write output to stdout\n");
 	printf("        a : cycle degrees of all vertices of each graph\n");
-	printf("        m : maximum cycle degree of each graph\n\n");
+	printf("        m : cycle degree of each graph\n\n");
 	printf("    -limit N: process the first N graphs in F\n\n");
 	printf("    -h | --help: display this help\n\n");
 }
@@ -42,6 +40,15 @@ int main(int argc, char** argv) {
 	} else {
 		int maxGraphs = -1;
 		char outputOption = 'a';
+		int param;
+		int i = 0;
+		struct Graph* g;
+
+		/* create object pools */
+		struct ListPool *lp = createListPool(1);
+		struct VertexPool *vp = createVertexPool(1);
+		struct ShallowGraphPool *sgp = createShallowGraphPool(1, lp);
+		struct GraphPool *gp = createGraphPool(1, vp, lp);
 
 		/* user input handling */
 		for (param=2; param<argc; param+=2) {
@@ -57,18 +64,8 @@ int main(int argc, char** argv) {
 			}
 		}
 
-		/* create object pools */
-		struct ListPool *lp = createListPool(1);
-		struct VertexPool *vp = createVertexPool(1);
-		struct ShallowGraphPool *sgp = createShallowGraphPool(1, lp);
-		struct GraphPool *gp = createGraphPool(1, vp, lp);
-
-		/* init params */
-		char* inputFileName = argv[1];	
-		FILE* stream = fopen(inputFileName, "r");
-		int bufferSize = 20;
-		int number;
-		struct ShallowGraph* patterns;
+		/* try to load a file */
+		createFileIterator(argv[1], gp);
 
 		/* iterate over all graphs in the database */
 		while (((i < maxGraphs) || (maxGraphs == -1)) && (g = iterateFile(&aids99VertexLabel, &aids99EdgeLabel))) {
@@ -76,9 +73,55 @@ int main(int argc, char** argv) {
 			/* if there was an error reading some graph the returned n will be -1 */
 			if (g->n > 0) {
 
-				
+				struct ShallowGraph* biconnectedComponents = listBiconnectedComponents(g, sgp);
+				int* occurrences = malloc(g->n * sizeof(int));
+				int* cycleDegrees = malloc(g->n * sizeof(int));
+			
+				int v;
+				struct ShallowGraph* comp;
+				int compNumber = 0;
 
+				for (v=0; v<g->n; ++v) {
+					occurrences[v] = -1;
+					cycleDegrees[v] = 0;
+				}
 
+				for (comp = biconnectedComponents; comp!=NULL; comp=comp->next) {
+					if (comp->m > 1) {
+						struct VertexList* e;
+						for (e=comp->edges; e!=NULL; e=e->next) {
+							if (occurrences[e->startPoint->number] < compNumber) {
+								occurrences[e->startPoint->number] = compNumber;
+								++cycleDegrees[e->startPoint->number];
+							}
+							if (occurrences[e->endPoint->number] < compNumber) {
+								occurrences[e->endPoint->number] = compNumber;
+								++cycleDegrees[e->endPoint->number];
+							}
+						}
+					}
+					++compNumber;
+				}
+
+				if (outputOption == 'a') {
+					for (v=0; v<g->n; ++v) {
+						fprintf(stdout, "%i ", cycleDegrees[v]);
+					}
+					fprintf(stdout, "\n");
+				}
+				if (outputOption == 'm') {
+					int maxDegree = 0;
+					for (v=0; v<g->n; ++v) {
+						if (maxDegree < cycleDegrees[v]) {
+							maxDegree = cycleDegrees[v];
+						}
+					}
+					fprintf(stdout, "%i\n", maxDegree);
+				}
+
+				free(occurrences);
+				free(cycleDegrees);
+				dumpShallowGraphCycle(sgp, biconnectedComponents);
 
 				/***** do not alter ****/
 				++i;
@@ -92,6 +135,7 @@ int main(int argc, char** argv) {
 		}
 
 		/* garbage collection */
+		destroyFileIterator();
 		freeGraphPool(gp);
 		freeShallowGraphPool(sgp);
 		freeListPool(lp);
