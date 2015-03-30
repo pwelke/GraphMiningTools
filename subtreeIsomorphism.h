32c91aa3bccecef22f3708338acc1340ab45a17d
Pascal Welke <pascal@xx>
2015-03-30 16:36:29 +0200

    merged subtreeIsomorphismLabeled and subtreeIsomorphism

--- subtreeIsomorphism.h
+++ subtreeIsomorphism.h
@@ -1,12 +1,19 @@
 #ifndef SUBTREE_ISOMORPHISM_H_
 #define SUBTREE_ISOMORPHISM_H_
 
-int*** createCube(int x, int y);
-void freeCube(int*** cube, int x, int y);
+#include "cachedGraph.h"
+
+// int*** createCube(int x, int y);
+// void freeCube(int*** cube, int x, int y);
 void dumpCube();
 
-int* findLeaves(struct Graph* g, int root);
-struct ShallowGraph* removeVertexFromBipartiteInstance(struct Graph* B, int v, struct ShallowGraphPool* sgp);
-void addVertexToBipartiteInstance(struct ShallowGraph* temp);
+// int* findLeaves(struct Graph* g, int root);
+// struct ShallowGraph* removeVertexFromBipartiteInstance(struct Graph* B, int v, struct ShallowGraphPool* sgp);
+// void addVertexToBipartiteInstance(struct ShallowGraph* temp);
+
+char subtreeCheck(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
+char subtreeCheckF(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
+char subtreeCheckFF(struct Graph* g, struct Graph* h, struct GraphPool* gp);
+char subtreeCheckCached(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct CachedGraph* cacheB);
 
 #endif /* SUBTREE_ISOMORPHISM_H_ */
--- subtreeIsomorphismLabeled.h
+++ subtreeIsomorphismLabeled.h
@@ -1,9 +1,6 @@
 #ifndef SUBTREE_ISOMORPHISM_H_
 #define SUBTREE_ISOMORPHISM_H_
 
-char subtreeCheck(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
-char subtreeCheckF(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
-char subtreeCheckFF(struct Graph* g, struct Graph* h, struct GraphPool* gp);
-char subtreeCheckCached(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct CachedGraph* cacheB);
+
 
 #endif /* SUBTREE_ISOMORPHISM_H_ */
