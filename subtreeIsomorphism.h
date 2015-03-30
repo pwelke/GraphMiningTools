82f61e2a885b63f8cbe73fa3a189b5a5122ec24f
Pascal Welke <pascal@xx>
2015-03-30 16:18:40 +0200

    removed all functions that are present in subtreeIsomorphismLabeled

--- subtreeIsomorphism.h
+++ subtreeIsomorphism.h
@@ -1,14 +1,11 @@
 #ifndef SUBTREE_ISOMORPHISM_H_
 #define SUBTREE_ISOMORPHISM_H_
 
-char subtreeCheck(struct Graph* g, struct Graph* h, struct GraphPool* gp, struct ShallowGraphPool* sgp);
-
 int*** createCube(int x, int y);
 void freeCube(int*** cube, int x, int y);
 void dumpCube();
 
 int* findLeaves(struct Graph* g, int root);
-struct ShallowGraph* removeVertexFromBipartiteInstance(struct Graph* B, int v, struct ShallowGraphPool* sgp);
 void addVertexToBipartiteInstance(struct ShallowGraph* temp);
 
 #endif /* SUBTREE_ISOMORPHISM_H_ */
