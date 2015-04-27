faf781527cf69e2ce5fea3e4c2be5b6a40be05eb
Pascal Welke <pascal@xx>
2015-04-27 12:52:54 +0200

    removed commented out stuff, removed obsolete imports

--- executables/levelwiseGraphMining.c
+++ executables/levelwiseGraphMining.c
@@ -7,14 +7,7 @@
 #include "../graph.h"
 #include "../searchTree.h"
 #include "../loading.h"
-#include "../listSpanningTrees.h"
-#include "../listComponents.h"
-#include "../upperBoundsForSpanningTrees.h"
-#include "../connectedComponents.h"
-#include "../cs_Tree.h"
-#include "../wilsonsAlgorithm.h"
 #include "../outerplanar.h"
-
 #include "../bloomFilter.h"
 #include "../cs_Parsing.h"
 
@@ -229,152 +222,6 @@ void getFrequentEdges(struct Graph** db, int dbSize, int initialResultSetSize, s
 }
 
 
-// /**
-// return the histogram of vertices and edges in a graph db in a search tree.
-// traverses the db once and stores the graphs in an array of correct length.
-
-// the db is expected to have the format outputted by printStringsInSearchTree().
-// both output parameters (frequent* ) should be initialized to "empty" structs.
-// // fileName specifies the file the db is contained in.
-
-// The method expects that initPruning was called with a positive argument before and returns 
-// the number of graphs in the database.
-// */
-// int getDBAndVertexAndEdgeHistograms( struct Vertex* frequentVertices, struct Vertex* frequentEdges, FILE* keyValueStream, struct GraphPool* gp, struct ShallowGraphPool* sgp) {
-// 	int i = 0;
-// 	struct compInfo* results = NULL;
-// 	int resultSize = 0;
-	
-// 	/* iterate over all graphs in the database */
-// 	while ((g = iterateFile(&intLabel, &intLabel))) {
-
-// 		int v;
-
-// 		/* frequency of an edge increases by one if there exists a pattern for the current graph (a spanning tree) 
-// 		that contains the edge. Thus we need to find all edges contained in any spanning tree and then add them 
-// 		to frequentEdges once omitting multiplicity */
-// 		struct Vertex* containedEdges = getVertex(gp->vertexPool);
-
-// 		/* the vertices contained in g can be obtained from a single spanning tree, as all spanning trees contain
-// 		the same vertex set. However, to omit multiplicity, we again resort to a temporary searchTree */
-// 		struct Vertex* containedVertices = getVertex(gp->vertexPool);
-
-// 		/* init temporary result storage if necessary */
-// 		int neededResultSize = g->m;
-// 		int resultPos = 0;
-// 		if (neededResultSize > resultSize) {
-// 			if (results) {
-// 				free(results);
-// 			}
-
-// 			results = getResultVector(neededResultSize);
-// 			resultSize = neededResultSize;
-// 		}
-
-
-
-
-// 		for (v=0; v<g->n; ++v) {
-// 			/* See commented out how it would look if done by the book.
-// 			However, this has to be fast and canonicalStringOfTree has
-// 			too much overhead!
-// 			    struct ShallowGraph* cString;
-// 			    auxiliary->vertices[0]->label = patternGraph->vertices[v]->label;
-// 			    cString = canonicalStringOfTree(auxiliary, sgp);
-// 			    addToSearchTree(containedVertices, cString, gp, sgp); */
-// 			struct VertexList* cString = getVertexList(sgp->listPool);
-// 			cString->label = g->vertices[v]->label;
-// 			containedVertices->d += addStringToSearchTree(containedVertices, cString, gp);
-// 			containedVertices->number += 1;
-// 		}
-// 		/* set multiplicity of patterns to 1 and add to global vertex pattern set, print to file */
-// 		resetToUnique(containedVertices);
-// 		mergeSearchTrees(frequentVertices, containedVertices, 1, results, &resultPos, frequentVertices, 0, gp);
-// 		dumpSearchTree(gp, containedVertices);
-
-// 		/* write (graph->number, pattern id) pairs to stream */
-// 		for (v=0; v<resultPos; ++v) {
-// 			fprintf(keyValueStream, "%i %i\n", g->number, results[v].id);
-// 		}
-		
-// 		/* get frequent Edges */
-// 		resultPos = 0;
-	
-// 		for (v=0; v<g->n; ++v) {
-// 			struct VertexList* e;
-// 			for (e=g->vertices[v]->neighborhood; e!=NULL; e=e->next) {
-// 				int w = e->endPoint->number;
-// 				/* edges occur twice in patternGraph. just add them once to the search tree */
-// 				if (w > v) {
-// 					/* as for vertices, I use specialized code to generate 
-// 					the canonical string of a single edge */
-// 					struct VertexList* cString;
-// 					if (strcmp(e->startPoint->label, e->endPoint->label) < 0) {
-// 						/* cString = v e (w) */
-// 						struct VertexList* tmp = getVertexList(gp->listPool);
-// 						tmp->label = e->endPoint->label;
-
-// 						cString = getTerminatorEdge(gp->listPool);
-// 						tmp->next = cString;
-
-// 						cString = getVertexList(gp->listPool);
-// 						cString->label = e->label;
-// 						cString->next = tmp;
-
-// 						tmp = getInitialisatorEdge(gp->listPool);
-// 						tmp->next = cString;
-
-// 						cString = getVertexList(gp->listPool);
-// 						cString->label = e->startPoint->label;
-// 						cString->next = tmp;
-// 					} else {
-// 						/* cString = w e (v) */
-// 						struct VertexList* tmp = getVertexList(gp->listPool);
-// 						tmp->label = e->startPoint->label;
-
-// 						cString = getTerminatorEdge(gp->listPool);
-// 						tmp->next = cString;
-
-// 						cString = getVertexList(gp->listPool);
-// 						cString->label = e->label;
-// 						cString->next = tmp;
-
-// 						tmp = getInitialisatorEdge(gp->listPool);
-// 						tmp->next = cString;
-
-// 						cString = getVertexList(gp->listPool);
-// 						cString->label = e->endPoint->label;
-// 						cString->next = tmp;
-// 					}
-// 					/* add the string to the search tree */
-// 					containedEdges->d += addStringToSearchTree(containedEdges, cString, gp);
-// 					containedEdges->number += 1;
-// 				} 
-// 			}
-// 		}
-		
-// 		/* set multiplicity of patterns to 1 and add to global edge pattern set */
-// 		resetToUnique(containedEdges);
-// 		mergeSearchTrees(frequentEdges, containedEdges, 1, results, &resultPos, frequentEdges, 0, gp);
-// 		dumpSearchTree(gp, containedEdges);
-		
-// 		/* write (graph->number, pattern id) pairs to stream, add the patterns to the bloom
-// 		filter of the graph (i) for pruning */
-// 		for (v=0; v<resultPos; ++v) {
-// 			fprintf(keyValueStream, "%i %i\n", g->number, results[v].id);
-// 			initialAddToPruningSet(results[v].id, i);
-// 		}
-		
-
-// 		/* counting of read graphs and garbage collection */
-// 		++i;
-// 	}
-// 	if (results != NULL) {
-// 		free(results);
-// 	}
-// 	return i;
-// }
-
 
 /**
  * Input handling, parsing of database and call of opk feature extraction method.
@@ -448,7 +295,7 @@ int main(int argc, char** argv) {
 		// fopen("/dev/null");
 		FILE* kvStream = fopen("/dev/null", "w");
 		FILE* featureStream = stdout;
-		FILE* patternStream = fopen("/dev/null", "w");
+		// FILE* patternStream = fopen("/dev/null", "w");
 		int i;
 
 		struct Vertex* frequentVertices = getVertex(vp);
@@ -464,16 +311,14 @@ int main(int argc, char** argv) {
 
 		/* get frequent vertices */
 		tmpResultSetSize = getFrequentVertices(db, nGraphs, frequentVertices, kvStream, gp);
+		printStringsInSearchTree(frequentVertices, featureStream, sgp);
 		filterSearchTreeP(frequentVertices, threshold, frequentVertices, featureStream, gp);
+		fprintf(featureStream, "\n");
 
-		fprintf(stdout, "\n");
 		/* get frequent edges: first edge id is given by number of frequent vertices */
-		offsetSearchTreeIds(frequentEdges, frequentVertices->d);
-		fprintf(stdout, "->d=%i\n", frequentEdges->d);
+		offsetSearchTreeIds(frequentEdges, frequentVertices->lowPoint);
 		getFrequentEdges(db, nGraphs, tmpResultSetSize, frequentEdges, kvStream, gp);
 		filterSearchTreeP(frequentEdges, threshold, frequentEdges, featureStream, gp);
-
-
 
 		// garbage collection
 		freePruning();
@@ -484,8 +329,6 @@ int main(int argc, char** argv) {
 			dumpGraph(gp, db[i]);
 		}
 		free(db);
-
-
 	}
 
 	/* global garbage collection */
