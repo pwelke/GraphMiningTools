058f4aa7b7ab54e6f5540dedc0c9bffa3c7e6139
Pascal Welke <pascal@xx>
2015-06-01 17:51:05 +0200

    implemented Kruskals Algorithm

---
+++ kruskalsAlgorithm.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+
+#include "graph.h"
+
+static struct Vertex* getRoot(struct Vertex* v) {
+	struct Vertex* r = v;
+	while (r->neighborhood != NULL) {
+		r = r->neighborhood->endPoint; 
+	}
+	return r;
+}
+
+static char isCycleFree(struct Graph* branching, struct VertexList* e, struct ListPool* lp) {
+	// translate vertices to vertices in branching
+	struct Vertex* v = branching->vertices[e->startPoint->number];
+	struct Vertex* w = branching->vertices[e->endPoint->number];
+	// find roots of components
+	struct Vertex* rv = getRoot(v);
+	struct Vertex* rw = getRoot(w);
+
+	if (rv == rw) {
+		return 0;
+	} else {
+		struct VertexList* e = getVertexList(lp);
+		if (rv->d < rw->d) {
+			e->startPoint = rv;
+			e->endPoint = rw;
+			addEdge(rv, e);
+			if (rv->d == rw->d) {
+				++rw->d;
+			}
+		} else {
+			e->startPoint = rw;
+			e->endPoint = rv;
+			addEdge(rw, e);
+			if (rv->d == rw->d) {
+				++rv->d;
+			}
+		}
+		return 1;
+	}
+}
+
+
+struct ShallowGraph* kruskal(struct Graph* g, struct VertexList** sortedEdges, struct GraphPool* gp, struct ShallowGraphPool* sgp) {
+	struct Graph* branching = createGraph(g->n, gp);
+	struct ShallowGraph* spanningTree = getShallowGraph(sgp);
+	int i;
+
+	for (i=0; i<g->m; ++i) {
+		if (isCycleFree(branching, sortedEdges[i])) {
+			appendEdge(spanningTree, shallowCopyEdge(sortedEdges[i], sgp->listPool));
+		}
+	}
+
+	dumpGraph(gp, branching);
+	return spanningTree;
+}
