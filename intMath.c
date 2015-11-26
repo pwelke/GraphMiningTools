4c9e3f5bd1347513e70352587fad7559852140ba
Pascal Welke <pascal@xx>
2015-11-26 11:17:46 +0100

    added max(int, int) to intMath.[ch]

--- intMath.c
+++ intMath.c
@@ -7,6 +7,13 @@ int min(int a, int b) {
 	return (a < b) ? a : b;
 }
 
+/** 
+Returns the larger of the two input values
+ */
+int max(int a, int b) {
+    return (a > b) ? a : b;
+}
+
 /*
  * returns the "correct" modulo of two numbers. b is assumed to be positive,
  * a is an arbitrary integer.
--- intMath.h
+++ intMath.h
@@ -2,6 +2,7 @@
 #define INT_MATH_H_
 
 int min(int a, int b);
+int max(int a, int b);
 int mod(int a, int b);
 int ipow(int b, int e);
 
