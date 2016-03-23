0c4961f948d7878922e7958b7faa1acbcf04ef85
pascal <pascal@taschenrechner>
2016-03-23 12:59:02 +0100

    Revert "cleanup of newCube"
    This reverts commit 368397c78d6246bfe1b99361f93248021b944b8f.
    
    Conflicts:
    	executables/iterativeSubtreeTest.c
    	executables/levelwiseGraphMining.c
    	newCube.c
    	newCube.h
    	subtreeIsoDataStoreList.c
    
    newCube.c and newCube.h will be dealt with in the next commit

--- newCube.c
+++ newCube.c
@@ -1,9 +1,15 @@
+#include "newCube.h"
+
 #include <assert.h>
 #include <malloc.h>
 
-#include "newCube.h"
+
+
 
 // CHARACTERISTICS TOOLING
+
+// it seems that bitcube has some bug resulting in false positives in the iterative subtree iso algorithm
+#ifdef INTCUBE
 
 // TODO can be made constant time
 /** Utility data structure creator.
@@ -61,6 +67,32 @@ void createNewCubeForSingletonPattern(struct SubtreeIsoDataStore* info) {
 }
 
 
+//void createNewCubeFromBaseFast_1(struct SubtreeIsoDataStore base, struct SubtreeIsoDataStore* new) {
+//	new->S = createNewCube(base.g->n, base.h->n + 1);
+//	new->elementsInS = 0;
+//	// create cube large enough for filtered characteristics plus new for old vertices of h
+//	// TODO I assume two things:
+//	// 1. we only need to add space for one more characteristic
+//	// 2. I think, we only need this additional space somewhere, not everywhere. Probably only for the neoghbor of the new vertex
+//	// 3. the new vertex can have only two characteristics. A complete and an incomplete for the unique parent.
+//	size_t maxSize = base.elementsInS; // space for characteristics of subgraph
+//	maxSize += base.g->n * (base.h->n + 1); // space for storing size info
+//	maxSize += base.g->n * base.h->n; // space for additional characteristic
+//	maxSize += 2 * base.g->n; // space for characteristics of new vertex
+//	int* array = calloc(maxSize, sizeof(int));
+//	size_t position = 0;
+//	for (int i=0; i<base.g->n; ++i) {
+//		for (int j=0; j<base.h->n; ++j) {
+//			new->S[i][j] = &(array[position]);
+//			//			new->S[i][j][0] = 0;
+//			position += base.S[i][j][0] + 2;
+//		}
+//		new->S[i][base.h->n] = &(array[position]);
+//		//		new->S[i][base.h->n][0] = 0;
+//		position += 3;
+//	}
+//}
+
 void createNewCubeFromBase(struct SubtreeIsoDataStore base, struct SubtreeIsoDataStore* new) {
 
 	// create cube large enough for filtered characteristics plus new for old vertices of h
@@ -107,6 +139,44 @@ void createNewCubeFromBase(struct SubtreeIsoDataStore base, struct SubtreeIsoDat
 }
 
 
+
+//void createNewCubeFromBaseFast_3(struct SubtreeIsoDataStore base, struct SubtreeIsoDataStore* new) {
+//	new->S = createNewCube(base.g->n, base.h->n + 1);
+//	new->elementsInS = 0;
+//	// create cube large enough for filtered characteristics plus new for old vertices of h
+//	// TODO I assume two things:
+//	// 1. we only need to add space for one more characteristic
+//	// 2. I think, we only need this additional space somewhere, not everywhere. Probably only for the neoghbor of the new vertex
+//	// 3. the new vertex can have only two characteristics. A complete and an incomplete for the unique parent.
+//	size_t maxSize = base.elementsInS; // space for characteristics of subgraph
+//	maxSize += base.g->n * (base.h->n + 1); // space for storing size info
+//	maxSize += base.g->n * base.h->n; // space for additional characteristic
+//	maxSize += 2 * base.g->n; // space for characteristics of new vertex
+//	int* array = calloc(maxSize, sizeof(int));
+//
+//	size_t* spaces = malloc(base.g->n * base.h->n * sizeof(size_t));
+//	int k = 0;
+//	for (int i=0; i<base.g->n; ++i) {
+//		for (int j=0; j<base.h->n; ++j) {
+//			spaces[k] = base.S[i][j][0] + 2;
+//			++k;
+//		}
+//	}
+//
+//	size_t position = 0;
+//	k = 0;
+//	for (int i=0; i<base.g->n; ++i) {
+//		for (int j=0; j<base.h->n; ++j) {
+//			new->S[i][j] = &(array[position]);
+//			position += spaces[k];
+//			++k;
+//		}
+//		new->S[i][base.h->n] = &(array[position]);
+//		position += 3;
+//	}
+//	free(spaces);
+//}
+
 void dumpNewCube(int*** S, int x) {
 	free(S[0][0]);
 	for (int i=0; i<x; ++i) {
@@ -115,6 +185,17 @@ void dumpNewCube(int*** S, int x) {
 	free(S);
 }
 
+
+
+//void dumpNewCube(int*** S, int x, int y) {
+//	for (int i=0; i<x; ++i) {
+//		for (int j=0; j<y; ++j) {
+//			free(S[i][j]);
+//		}
+//		free(S[i]);
+//	}
+//	free(S);
+//}
 
 int* rawCharacteristics(struct SubtreeIsoDataStore data, struct Vertex* u, struct Vertex* v) {
 	return data.S[v->number][u->number];
@@ -129,6 +210,16 @@ int containsCharacteristic(struct SubtreeIsoDataStore data, struct Vertex* y, st
 	}
 	return 0;
 }
+
+
+//int containsCharacteristic_2(struct SubtreeIsoDataStore data, struct Vertex* y, struct Vertex* u, struct Vertex* v) {
+//	int uvNumberOfCharacteristics = data.S[v->number][u->number][0];
+//	int value = 0;
+//	for (int i=1; i<=uvNumberOfCharacteristics; ++i) {
+//		value |= y->number == data.S[v->number][u->number][i];
+//	}
+//	return value;
+//}
 
 
 char checkSanityOfWrite(struct SubtreeIsoDataStore* data, struct Vertex* u, struct Vertex* v) {
@@ -236,3 +327,38 @@ void testNewCubeSizes(int*** S, int gn, int hn) {
 	}
 	printf("has %i entries\n", size);
 }
+
+#endif
+
+#ifdef BITCUBE
+
+char* createNewCube(int x, int y) {
+	return createBitset(x * y * y);
+}
+
+void createNewBaseCubeFast(struct SubtreeIsoDataStore* info) {
+	info->S = createNewCube(info->g->n, 2);
+}
+
+
+void createNewCubeFromBaseFast(struct SubtreeIsoDataStore base, struct SubtreeIsoDataStore* new) {
+	new->S = createNewCube(base.g->n, base.h->n + 1);
+}
+
+void dumpNewCubeFast(char* S, int x, int y) {
+	destroyBitset(S);
+}
+
+int containsCharacteristic(struct SubtreeIsoDataStore data, struct Vertex* y, struct Vertex* u, struct Vertex* v) {
+	return getBit(data.S, ((v->number * data.h->n) + u->number) * data.h->n + y->number);
+}
+
+
+void addCharacteristic(struct SubtreeIsoDataStore* data, struct Vertex* y, struct Vertex* u, struct Vertex* v) {
+	setBitTrue(data->S, ((v->number * data->h->n) + u->number) * data->h->n + y->number);
+}
+
+#endif
+
+
+
--- newCube.h
+++ newCube.h
@@ -13,9 +13,14 @@
 #include "bitSet.h"
 #include "graph.h"
 
+// #define INTCUBE
+#define INTCUBE
+
+#ifdef INTCUBE
 // CHARACTERISTICS TOOLING
 
 struct SubtreeIsoDataStore {
+	// TODO can be moved out to save 128bit per graph.
 	int* postorder;
 	struct Graph* g;
 
@@ -41,5 +46,36 @@ void printNewCube(int*** S, int gn, int hn);
 void printNewCubeCondensed(int*** S, int gn, int hn);
 
 void testNewCubeSizes(int*** S, int gn, int hn);
+
+#endif
+
+#ifdef BITCUBE
+
+struct SubtreeIsoDataStore {
+	// TODO can be moved out to save 128bit per graph.
+	int* postorder;
+	struct Graph* g;
+
+	struct Graph* h;
+//	int*** S;
+	char* S;
+	size_t elementsInS;
+	int foundIso;
+};
+
+/** Utility data structure creator.
+Cube will store, what is called S in the paper. */
+char* createNewCube(int x, int y);
+void createNewBaseCubeFast(struct SubtreeIsoDataStore* info);
+void createNewCubeFromBaseFast(struct SubtreeIsoDataStore base, struct SubtreeIsoDataStore* new);
+void dumpNewCubeFast(char* S, int x, int y);
+int containsCharacteristic(struct SubtreeIsoDataStore data, struct Vertex* y, struct Vertex* u, struct Vertex* v);
+char checkSanityOfWrite(struct SubtreeIsoDataStore* data, struct Vertex* u, struct Vertex* v);
+// assumes that
+void addCharacteristic(struct SubtreeIsoDataStore* data, struct Vertex* y, struct Vertex* u, struct Vertex* v);
+/** Print a single entry in the cube */
+#endif
+
+
 
 #endif /* NEWCUBE_H_ */
