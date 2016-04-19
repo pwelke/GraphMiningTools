1545c07727cbf8ce0781fa882070bf775cb3f914
Pascal Welke <pascal@xx>
2016-04-19 15:53:16 +0200

    compute a postorder for disconnected graphs (subtreeIso dependency)

--- subtreeIsoUtils.c
+++ subtreeIsoUtils.c
@@ -23,12 +23,11 @@ int labelCmp(const char* l1, const char* l2) {
 
 /** dfs function local to getPostorder() */
 static int dfs(struct Vertex* v, int value) {
-	struct VertexList* e;
 
 	/* to make this method save for graphs that are not trees */
 	v->visited = -2;
 
-	for (e=v->neighborhood; e!=NULL; e=e->next) {
+	for (struct VertexList* e=v->neighborhood; e!=NULL; e=e->next) {
 		if (e->endPoint->visited == -1) {
 			value = 1 + dfs(e->endPoint, value);
 			e->endPoint->lowPoint = v->number;
@@ -47,7 +46,7 @@ The method returns an array of length g->n where position i contains the vertex 
 of the ith vertex in the order.
 The ->lowPoint s of vertices in g point to their parents in the postorder.
 */
-int* getPostorder(struct Graph* g, int root) {
+int* getPostorderOfTree(struct Graph* g, int root) {
 	int* order = malloc(g->n * sizeof(int));
 	for (int i=0; i<g->n; ++i) {
 		g->vertices[i]->visited = -1;
@@ -68,17 +67,42 @@ int* getPostorder(struct Graph* g, int root) {
 
 
 /**
+Compute a dfs order or postorder on g starting at vertex root.
+The ->visited members of vertices are set to the position they have in the order
+(starting with 0).
+The method returns an array of length g->n where position i contains the vertex number
+of the ith vertex in the order.
+The ->lowPoint s of vertices in g point to their parents in the postorder.
+
+If g is disconnected, another dfs is started for each connected component that does not contain root.
+For each of those components, the root is the vertex in that component with the lowest ->number.
+*/
+int* getPostorder(struct Graph* g, int root) {
+	int* order = malloc(g->n * sizeof(int));
+	for (int i=0; i<g->n; ++i) {
+		g->vertices[i]->visited = -1;
+		order[i] = -1;
+	}
+	int nVisited = dfs(g->vertices[root], 0);
+	g->vertices[root]->lowPoint = -1;
+	for (int i=0; i<g->n; ++i) {
+		if (g->vertices[i]->visited == -1) {
+			nVisited = dfs(g->vertices[i], nVisited + 1);
+		}
+		order[g->vertices[i]->visited] = i;
+	}
+	return order;
+}
+
+
+/**
 Find all vertices reachable by augmenting paths that start with a non-matching edge
 */
 void markReachable(struct Vertex* a, int num) {
-	struct VertexList* e;
 
 	a->visited = 1;
-	// if (a->number < num)
-	// printf("Mark %i visited\n", a->lowPoint);
 
-	for (e=a->neighborhood; e!=NULL; e=e->next) {
-		// printf("check edge (%i %i) flag %i\n", e->startPoint->lowPoint, e->endPoint->lowPoint, e->flag);
+	for (struct VertexList* e=a->neighborhood; e!=NULL; e=e->next) {
 		if ((e->flag == 0) && (e->endPoint->visited == 0)) {
 			markReachable(e->endPoint, num);
 		}
