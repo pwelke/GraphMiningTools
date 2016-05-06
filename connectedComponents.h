5b74334edd44ceff012a243f391ed30477fd3897
Pascal Welke <welke@kdml02.iai.uni-bonn.de>
2016-05-06 14:31:34 +0200

    publish markComponents method

--- connectedComponents.h
+++ connectedComponents.h
@@ -4,5 +4,6 @@
 struct ShallowGraph* getConnectedComponents(struct Graph* g, struct ShallowGraphPool* sgp);
 struct ShallowGraph* getRepresentativeVertices(struct Graph* g, struct ShallowGraphPool* sgp);
 char isConnected(struct Graph* g);
+void markComp(struct Vertex* v, int compNumber);
 
 #endif
