322c914ce96b5b0acb5476ea2d1c8132def6898d
Pascal Welke <pascal@xx>
2016-09-27 08:35:30 +0200

    add header localEasySubtreeIsomorphism.h

---
+++ localEasySubtreeIsomorphism.h
@@ -0,0 +1,44 @@
+#ifndef LOCAL_EASY_SUBTREE_ISO
+#define LOCAL_EASY_SUBTREE_ISO
+
+#include "subtreeIsoDataStoreList.h"
+
+/*
+ want a tree on roots.
+ edges should go from parent to child
+ [x] each vertex should store its parent vertex (where, in g or in the tree?)
+ [x] each vertex should store all its v-rooted components
+*/
+struct BlockTree{
+	struct Graph* g;
+	struct Vertex** roots;
+	struct Vertex** parents;
+	struct ShallowGraph** vRootedBlocks;
+	int nRoots;
+};
+
+/*
+ *  [x] each vertex should store spanning trees of the v-rooted components
+ *  each vertex should store the set of characteristics
+ *  and maybe some pruning info?...
+ */
+struct SpanningtreeTree{
+	struct Graph* g;
+	struct Vertex** roots;
+	struct Vertex** parents;
+	struct Graph** localSpanningTrees;
+	struct SubtreeIsoDataStoreList** characteristics;
+//	struct Graph** blocks;
+//	struct ShallowGraph** blockSpanningTrees;
+	int nRoots;
+};
+
+
+struct BlockTree getBlockTreeT(struct Graph* g, struct ShallowGraphPool* sgp);
+struct Graph* blockConverter(struct ShallowGraph* edgeList, struct GraphPool* gp);
+struct Graph* spanningTreeConverter(struct ShallowGraph* localTrees, struct Graph* component, struct GraphPool* gp, struct ShallowGraphPool* sgp);
+struct SpanningtreeTree getSpanningtreeTree(struct BlockTree blockTree, int spanningTreesPerBlock, struct GraphPool* gp, struct ShallowGraphPool* sgp);
+
+char noniterativeLocalEasySubtreeCheck(struct SpanningtreeTree sptTree, struct Graph* h, struct GraphPool* gp);
+
+#endif
