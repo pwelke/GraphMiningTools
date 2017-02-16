f8f1d814502633f59727bdea787d0a974569a1f6
Pascal Welke <pascal@xx>
2017-02-16 17:21:31 +0100

    work on the min path cover algorithm

--- poset_pathCover.c
+++ poset_pathCover.c
@@ -86,4 +86,150 @@ struct Graph* createVertexCoverFlowInstanceOfPoset(struct Graph* g, struct Graph
 }
 
 
+static int* getPathInFlowInstance(struct Vertex* v) {
+
+	// DFS without backtracking in DAG
+	int pathLength = 2;
+	for (struct VertexList* e=v->neighborhood; e!=NULL && (e->flag > 0); e=e->endPoint->neighborhood) {
+		++pathLength;
+	}
+
+	// debug
+	if (pathLength % 2 == 1) {
+		fprintf(stderr, "Pathlength is odd. This must not happen.\n");
+	}
+
+	int* path = malloc((pathLength / 2) * sizeof(int));
+	path[0] = pathLength / 2;
+	path[1] = v->number;
+
+	pathLength = 2;
+	// DFS without backtracking in DAG
+	--v->visited;
+	for (struct VertexList* e=v->neighborhood; e!=NULL && (e->flag > 0); e=e->endPoint->neighborhood) {
+		--e->flag;
+		--e->endPoint->visited;
+		path[pathLength] = e->endPoint->number;
+		++pathLength;
+	}
+
+	return path;
+}
+
+//static int* getLongestPathInFlowInstance(struct Vertex* v, struct Graph* poset, struct ShallowGraphPool* sgp) {
+//
+//	struct ShallowGraph* border = getShallowGraph(sgp);
+//	addToVertexQueue(v, border, sgp);
+//	v->lowPoint = -1;
+//	v->d = 1;
+//
+//	// use ->lowPoint to store the a parent of w
+//	// for sake of simplicity we count the number of times a vertex is in the border and
+//	struct Vertex* highestVertex = v;
+//	for (struct Vertex* w=popFromVertexQueue(border, sgp); w!=NULL;  w=popFromVertexQueue(border, sgp)) {
+//		highestVertex = w;
+//		w->d -= 1;
+//		if (w->d == 0) {
+//			for (struct VertexList* e=w->neighborhood; e!=NULL; e=e->next) {
+//				if ((e->endPoint->visited == 0) && (e->endPoint->d == 0)) {
+//					e->endPoint->lowPoint = w->number;
+//					e->endPoint->d += 1;
+//					addToVertexQueue(e->endPoint, border, sgp);
+//				}
+//			}
+//		}
+//	}
+//	// here w stores the last visited pattern that, by construction, is on the highest level of the poset.
+//	// backtrack using the ->lowPoints from here.
+//	int pathLength = 2;
+//	int* path = NULL;
+//	for (struct Vertex* x=highestVertex; x->lowPoint!=-1; x=poset->vertices[x->lowPoint]) {
+//		++pathLength;
+//	}
+//
+//	path = malloc(pathLength * sizeof(int));
+//	path[0] = pathLength;
+//	path[1] = v->number;
+//
+//	pathLength -= 1;
+//	for (struct Vertex* x=highestVertex; x->lowPoint!=-1; x=poset->vertices[x->lowPoint]) {
+//		path[pathLength] = x->number;
+//		--pathLength;
+//	}
+//
+//	return path;
+//}
+
+
+
+/**
+ *
+ * Assumes that edges in the original poset are guaranteed to go from smaller vertex->number to larger vertex->number.
+ * Also assumes that the poset starts with an artificial vertex at position 0 that points to the smallest elements, but
+ * is not a part of the poset.
+ */
+struct ShallowGraph* getPathCoverOfPoset(struct Graph* g, struct GraphPool* gp, struct ShallowGraphPool* sgp) {
+
+	struct Graph* flowInstance = createVertexCoverFlowInstanceOfPoset(g, gp);
+
+	// find max number of s-t paths
+	int nAugmentations = 0;
+	while (augmentWithCapacity(flowInstance->vertices[0], flowInstance->vertices[1])) {
+		++nAugmentations;
+	}
+
+	// add one flow value to those edges that correspond to vertices
+	for (int v=2; v<flowInstance->n; v+=2) {
+		for (struct VertexList* e=flowInstance->vertices[v]->neighborhood; e!=NULL; e=e->next) {
+			if (e->endPoint->number == v+1) {
+				e->flag += 1;
+				break; // iterating over the neighbors
+			}
+		}
+	}
+
+	// remove all edges that have 0 flow, all residual edges (assumed to go from larger vertex id to smaller),
+	// and all edges containing s and t
+	// init ->visited of vertices
+	dumpVertexList(gp->listPool, flowInstance->vertices[0]->neighborhood);
+	dumpVertexList(gp->listPool, flowInstance->vertices[1]->neighborhood);
+	for (int v=2; v<flowInstance->n; ++v) {
+		struct VertexList* keep = NULL;
+		flowInstance->vertices[v]->visited = 0;
+		for (struct VertexList* e=flowInstance->vertices[v]->neighborhood; e!=NULL; e=flowInstance->vertices[v]->neighborhood) {
+			flowInstance->vertices[v]->neighborhood = e->next;
+			if ((e->flag != 0) || (e->startPoint->number > e->endPoint->number)) {
+				e->next = keep;
+				keep = e;
+			} else {
+				dumpVertexList(gp->listPool, e);
+			}
+		}
+		flowInstance->vertices[v]->neighborhood = keep;
+	}
+
+	// store outdegree - indegree in v->visited
+	for (int v=2; v<flowInstance->n; ++v) {
+		for (struct VertexList* e=flowInstance->vertices[v]->neighborhood; e!=NULL; e=e->next) {
+			e->startPoint->visited += 1;
+			e->endPoint->visited -= 1;
+		}
+	}
+
+	// how many paths are there?
+	for (int v=2; v<)
+
+	// find paths from sources to sinks
+	struct ShallowGraph* paths = NULL;
+	for (int v=2; v<flowInstance->n; ++v) {
+		while (flowInstance->vertices[v]->visited > 0) {
+
+		}
+	}
+
+	dumpGraph(gp, flowInstance);
+	return paths;
+}
+
+
 
